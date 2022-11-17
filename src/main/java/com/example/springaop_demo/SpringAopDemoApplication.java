2d16cf5df86783154dcaf27efc2cc10af7dd025c
varshap-17 <varshap630@gmail.com>
2022-11-17 21:12:25 +0530

    application main class

---
+++ src/main/java/com/example/springaop_demo/SpringAopDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.springaop_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+public class SpringAopDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopDemoApplication.class, args);
+    }
+
+}
